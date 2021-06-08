037dbf6eb2873e1b9644f597ccf49d324538b708
NoeliaCq <69400198+NoeliaCq@users.noreply.github.com>
2021-06-08 19:54:26 -0300

    Requerimientos 1, 2 y 3 para Tarea Simple con Test

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,21 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+import java.lang.Math.ceil
+
+interface Tarea {
+    val horasEstimadas : Double
+    fun horasNecesarias() : Double
+    fun nominaEmpleados() : MutableList<Empleado>
+    fun costo() : Double
+}
+
+class TareaSimple(override val horasEstimadas: Double, val nominaEmpleados:MutableList<Empleado>, val costoInfra: Double
+) : Tarea{
+
+    override fun horasNecesarias() : Double = ceil(horasEstimadas / (nominaEmpleados.size - 1))
+
+    override fun nominaEmpleados(): MutableList<Empleado> = nominaEmpleados
+
+    override fun costo(): Double = nominaEmpleados.sumBy { it.sueldoPorTarea(this).toInt() } + costoInfra
+}
+
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,23 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
-  describe("Una tarea") {
+  //Empleados varios
+  val responsable1 = Empleado(1500, Responsable)
+  val desarrollador = Empleado(500, EmpleadoComun)
+  val analista = Empleado(700, EmpleadoComun)
+  val diseniador = Empleado(500, EmpleadoComun)
+
+  describe("Una tarea Simple") {
+    val tareaFacil = TareaSimple(200.0, mutableListOf(responsable1, desarrollador, analista, diseniador), 20000.0)
+
+    it("nomina de empleados, horas necesarias para la tarea y costo") {
+      tareaFacil.nominaEmpleados().shouldContainExactly(responsable1, desarrollador, analista, diseniador)
+      tareaFacil.horasNecesarias().shouldBe(67.0)
+      tareaFacil.costo().shouldBe(433900.0)
+    }
   }
 })
